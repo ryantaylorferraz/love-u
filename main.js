02222eda8825654ed5462ad5ec70917d78fc2bd6
ryan <ryan.bilac2010@hotmail.com>
2023-08-22 11:23:18 -0300

    adicionei JS

---
+++ main.js
@@ -0,0 +1,7 @@
+const entradaName = prompt("Insira seu nome")
+    if(entradaName === "Isabela") {
+        alert(`Olá, ${entradaName}, seja bem-vinda`)
+    } else {
+        alert(`Você não é a Isabela! Impostora!`)
+        alert(`Recarregue a página e Digite o nome correto!`)
+    }
